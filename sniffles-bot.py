06f9d648b2ea06a667e882b2bbd469b2f94467b5
Branden Livermore <branden.livermore@gmail.com>
2018-03-01 20:45:59 -0800

    syntax

--- sniffles-bot.py
+++ sniffles-bot.py
@@ -23,7 +23,7 @@ async def price(*, query: str):
 	for result in results:
 		text += '{name}: {price}, today\'s change: {change}, alch price: {alch_price}\n'.format(name=result.name, price=human_format(result.price), change=human_format(result.change), alch_price=human_format(result.alch_price))
 	
-	if results is empty:
+	if not results:
 		text = 'nothing found for that shit'
 	
 	await bot.say(text)
